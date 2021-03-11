340d7e5322e22bbc4ec1dca7740a4a4b7c05cf12
Kirill <npabllla@gmail.com>
2021-03-11 21:21:36 +0300

    3. Класс хранилища пользователей UserStorage [#492680]

--- src/main/java/ru/job4j/storage/UserStorage.java
+++ src/main/java/ru/job4j/storage/UserStorage.java
@@ -6,6 +6,7 @@ import net.jcip.annotations.ThreadSafe;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @ThreadSafe
 public class UserStorage {
@@ -13,7 +14,7 @@ public class UserStorage {
     private final Map<Integer, User> users = new HashMap<>();
 
     public synchronized boolean add(User user) {
-        if (!users.containsKey(user.getId())) {
+        if (users.get(user.getId()) == null) {
             users.put(user.getId(), user);
             return true;
         } else {
@@ -22,7 +23,7 @@ public class UserStorage {
     }
 
     public synchronized boolean update(User user) {
-        if (delete(user)) {
+        if (users.containsKey(user.getId())) {
             users.put(user.getId(), user);
             return true;
         } else {
@@ -37,13 +38,9 @@ public class UserStorage {
     public synchronized void transfer(int fromId, int told, int amount) {
         User userFrom = users.get(fromId);
         User userTo = users.get(told);
-        if (userFrom != null && userTo != null) {
-            users.remove(userFrom.getId());
-            users.remove(userTo.getId());
+        if (userFrom != null && userTo != null && userFrom.getAmount() >= amount) {
             users.put(fromId, new User(userFrom.getId(), userFrom.getAmount() - amount));
             users.put(told, new User(userTo.getId(), userTo.getAmount() + amount));
-        } else {
-            throw new NoSuchElementException();
         }
     }
 }
