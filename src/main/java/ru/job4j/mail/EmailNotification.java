74f8d2e6a950a78e1b95ef1b8d1e96cccfab6ad8
Kirill Boblak <kaboblak@miem.hse.ru>
2021-03-12 18:30:58 +0300

    2. ExecutorService рассылка почты. [#492653]

---
+++ src/main/java/ru/job4j/mail/EmailNotification.java
@@ -0,0 +1,32 @@
+package ru.job4j.mail;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user){
+        String subject = "subject = Notification " + user.getUsername()
+                + " to email " + user.getEmail();
+        String body = "body = Add a new event to " + user.getUsername();
+        pool.submit(() -> send(subject, body, user.getEmail()));
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+}
